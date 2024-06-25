36909610e2fbb7df667a24d8b7ddc47e90473d6e
smix8 <52464204+smix8@users.noreply.github.com>
2024-06-25 04:50:43 +0200

    Add navigation obstacles to performance monitor stats
    Adds navigation obstacle count to navigation performance monitor.

--- modules/navigation/nav_map.h
+++ modules/navigation/nav_map.h
@@ -124,6 +124,7 @@ class NavMap : public NavRid {
 	int pm_edge_merge_count = 0;
 	int pm_edge_connection_count = 0;
 	int pm_edge_free_count = 0;
+	int pm_obstacle_count = 0;
 
 	struct ConnectionPair {
 		gd::Edge::Connection connections[2];
@@ -227,6 +228,7 @@ public:
 	int get_pm_edge_merge_count() const { return pm_edge_merge_count; }
 	int get_pm_edge_connection_count() const { return pm_edge_connection_count; }
 	int get_pm_edge_free_count() const { return pm_edge_free_count; }
+	int get_pm_obstacle_count() const { return pm_obstacle_count; }
 
 	int get_region_connections_count(NavRegion *p_region) const;
 	Vector3 get_region_connection_pathway_start(NavRegion *p_region, int p_connection_id) const;
