cd7ef651d6517d9553b99af07dadb842ab62dc52
tetrapod00 <145553014+tetrapod00@users.noreply.github.com>
2024-09-14 16:13:18 -0700

    Use double comments in shader template light functions

--- editor/shader_create_dialog.cpp
+++ editor/shader_create_dialog.cpp
@@ -170,8 +170,8 @@ void fragment() {
 }
 
 //void light() {
-	// Called for every pixel for every light affecting the material.
-	// Uncomment to replace the default light processing function with this one.
+//	// Called for every pixel for every light affecting the material.
+//	// Uncomment to replace the default light processing function with this one.
 //}
 )";
 						break;
@@ -186,8 +186,8 @@ void fragment() {
 }
 
 //void light() {
-	// Called for every pixel for every light affecting the CanvasItem.
-	// Uncomment to replace the default light processing function with this one.
+//	// Called for every pixel for every light affecting the CanvasItem.
+//	// Uncomment to replace the default light processing function with this one.
 //}
 )";
 						break;
