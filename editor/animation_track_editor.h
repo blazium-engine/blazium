1a8ba104fa48ae5aeb5a21bec8f802303c3064e2
Silc Lizard (Tokage) Renew <61938263+TokageItLab@users.noreply.github.com>
2024-11-17 01:54:06 +0900

    Fix spinner in AnimationTrackEdit in FPS mode

--- editor/animation_track_editor.h
+++ editor/animation_track_editor.h
@@ -980,6 +980,7 @@ class AnimationTrackKeyEditEditor : public EditorProperty {
 		Variant value;
 	} key_data_cache;
 
+	void _time_edit_spun();
 	void _time_edit_entered();
 	void _time_edit_exited();
 
@@ -999,7 +1000,6 @@ class AnimationMarkerKeyEditEditor : public EditorProperty {
 
 	EditorSpinSlider *spinner = nullptr;
 
-	void _time_edit_entered();
 	void _time_edit_exited();
 
 public:
