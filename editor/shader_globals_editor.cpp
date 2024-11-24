ceb9a45c000f8ccfc393ef8f3966c41b2b8ec152
Chaosus <chaosus89@gmail.com>
2024-11-24 14:18:53 +0300

    Add `samplerExternalOES` type to shader globals

--- editor/shader_globals_editor.cpp
+++ editor/shader_globals_editor.cpp
@@ -69,6 +69,7 @@ static const char *global_var_type_names[RS::GLOBAL_VAR_TYPE_MAX] = {
 	"sampler2DArray",
 	"sampler3D",
 	"samplerCube",
+	"samplerExternalOES",
 };
 
 class ShaderGlobalsEditorInterface : public Object {
@@ -236,6 +237,11 @@ protected:
 					pinfo.hint = PROPERTY_HINT_RESOURCE_TYPE;
 					pinfo.hint_string = "Cubemap,CompressedCubemap";
 				} break;
+				case RS::GLOBAL_VAR_TYPE_SAMPLEREXT: {
+					pinfo.type = Variant::OBJECT;
+					pinfo.hint = PROPERTY_HINT_RESOURCE_TYPE;
+					pinfo.hint_string = "ExternalTexture";
+				} break;
 				default: {
 				} break;
 			}
@@ -341,6 +347,9 @@ static Variant create_var(RS::GlobalShaderParameterType p_type) {
 			return "";
 		}
 		case RS::GLOBAL_VAR_TYPE_SAMPLERCUBE: {
+			return "";
+		}
+		case RS::GLOBAL_VAR_TYPE_SAMPLEREXT: {
 			return "";
 		}
 		default: {
