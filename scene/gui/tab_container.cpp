aa1746c9bc798d72a5be2c81d6aa7ba4113f8f3b
Janis Kirsteins <janis.kirsteins@icloud.com>
2025-01-19 00:33:39 +0100

    Typo fix: rename SortableVisbilityMode

--- scene/gui/tab_container.cpp
+++ scene/gui/tab_container.cpp
@@ -366,7 +366,7 @@ void TabContainer::_on_mouse_exited() {
 Vector<Control *> TabContainer::_get_tab_controls() const {
 	Vector<Control *> controls;
 	for (int i = 0; i < get_child_count(); i++) {
-		Control *control = as_sortable_control(get_child(i), SortableVisbilityMode::IGNORE);
+		Control *control = as_sortable_control(get_child(i), SortableVisibilityMode::IGNORE);
 		if (!control || control == tab_bar || children_removing.has(control)) {
 			continue;
 		}
@@ -543,7 +543,7 @@ void TabContainer::add_child_notify(Node *p_child) {
 		return;
 	}
 
-	Control *c = as_sortable_control(p_child, SortableVisbilityMode::IGNORE);
+	Control *c = as_sortable_control(p_child, SortableVisibilityMode::IGNORE);
 	if (!c) {
 		return;
 	}
@@ -573,7 +573,7 @@ void TabContainer::move_child_notify(Node *p_child) {
 		return;
 	}
 
-	Control *c = as_sortable_control(p_child, SortableVisbilityMode::IGNORE);
+	Control *c = as_sortable_control(p_child, SortableVisibilityMode::IGNORE);
 	if (c) {
 		tab_bar->move_tab(c->get_meta("_tab_index"), get_tab_idx_from_control(c));
 	}
@@ -588,7 +588,7 @@ void TabContainer::remove_child_notify(Node *p_child) {
 		return;
 	}
 
-	Control *c = as_sortable_control(p_child, SortableVisbilityMode::IGNORE);
+	Control *c = as_sortable_control(p_child, SortableVisibilityMode::IGNORE);
 	if (!c) {
 		return;
 	}
