504899114edb26c2e13b84ab81d11ca1f4a2c6d2
Malcolm Anderson <malcolminyo@gmail.com>
2025-01-26 19:28:57 -0800

    Add UID upgrade tool
    Apply suggestions from code review
    
    memdelete the UID upgrade tool
    
    Remove redeclaration of singleton
    
    Add note about committing .uid files to version control
    
    Add "Learn more" button that links to Godot blog post about UIDs
    
    Detect project from 4.3 or less and automatically display UID upgrade window
    
    Display popup after first run of `_sources_changed`
    
    Apply suggestions from code review
    
    Co-authored-by: Hugo Locurcio <hugo.locurcio@hugo.pro>
    Co-authored-by: Tomasz Chabora <kobewi4e@gmail.com>
    
    Replace magic strings with constants
    
    Update editor/editor_node.cpp
    
    Co-authored-by: Tomasz Chabora <kobewi4e@gmail.com>

--- editor/editor_node.h
+++ editor/editor_node.h
@@ -103,6 +103,8 @@ class ProjectSettingsEditor;
 class SceneImportSettingsDialog;
 class SurfaceUpgradeTool;
 class SurfaceUpgradeDialog;
+class UIDUpgradeTool;
+class UIDUpgradeDialog;
 
 struct EditorProgress {
 	String task;
@@ -169,6 +171,7 @@ public:
 		TOOLS_ORPHAN_RESOURCES,
 		TOOLS_BUILD_PROFILE_MANAGER,
 		TOOLS_SURFACE_UPGRADE,
+		TOOLS_UID_UPGRADE,
 		TOOLS_CUSTOM,
 
 		VCS_METADATA,
@@ -464,7 +467,14 @@ private:
 
 	SurfaceUpgradeTool *surface_upgrade_tool = nullptr;
 	SurfaceUpgradeDialog *surface_upgrade_dialog = nullptr;
+
 	bool run_surface_upgrade_tool = false;
+
+	UIDUpgradeTool *uid_upgrade_tool = nullptr;
+	UIDUpgradeDialog *uid_upgrade_dialog = nullptr;
+
+	bool run_uid_upgrade_tool = false;
+	bool should_prompt_uid_upgrade_tool = false;
 
 	bool was_window_windowed_last = false;
 
