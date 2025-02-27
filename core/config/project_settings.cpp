66e6988b86b7e67fb7b42e94ed5f23ca66d18766
Rémi Verschelde <rverschelde@gmail.com>
2025-02-27 17:25:34 +0100

    Input: Change default deadzone back to 0.5 for `ui_*` actions and axis `pressed` state
    Fixes #103360.
    Partial revert of changes in #97281 and #99135.

--- core/config/project_settings.cpp
+++ core/config/project_settings.cpp
@@ -1460,7 +1460,7 @@ void ProjectSettings::_add_builtin_input_map() {
 			}
 
 			Dictionary action;
-			action["deadzone"] = Variant(InputMap::DEFAULT_DEADZONE);
+			action["deadzone"] = Variant(InputMap::DEFAULT_TOGGLE_DEADZONE);
 			action["events"] = events;
 
 			String action_name = "input/" + E.key;
