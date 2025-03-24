5a84657f11b7b0d0e807525c9296680c9670ae54
Whales State <whalesstate@gmail.com>
2025-03-24 13:26:58 +0200

    Fix web export infinite reload issue
    Fix commit 28f23ba

--- platform/web/export/export_plugin.cpp
+++ platform/web/export/export_plugin.cpp
@@ -358,6 +358,9 @@ Error EditorExportPlatformWeb::_build_js_files(const Ref<EditorExportPreset> &p_
 }
 
 Error EditorExportPlatformWeb::_build_pwa(const Ref<EditorExportPreset> &p_preset, const String p_path, const Vector<SharedObject> &p_shared_objects) {
+	List<String> preset_features;
+	get_preset_features(p_preset, &preset_features);
+
 	String proj_name = GLOBAL_GET("application/config/name");
 	if (proj_name.is_empty()) {
 		proj_name = "Blazium Game";
@@ -384,6 +387,9 @@ Error EditorExportPlatformWeb::_build_pwa(const Ref<EditorExportPreset> &p_prese
 		cache_files.push_back(name + ".apple-touch-icon.png");
 	}
 
+	if (preset_features.find("threads")) {
+		cache_files.push_back(name + ".worker.js");
+	}
 	cache_files.push_back(name + ".audio.worklet.js");
 	cache_files.push_back(name + ".audio.position.worklet.js");
 	replaces["___GODOT_CACHE___"] = Variant(cache_files).to_json_string();
