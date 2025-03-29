picoxr/godot

Enable XR Preview Mode for PICO OS
Supported features in the immersive preview mode
- Passthrough
- Hand tracking
Notes:
- Multi-window is only available for internal apps on PICO OS, therefore launching adjacent windows is not possible currently.
- Passthrough splash screen is not supported on PICO OS

platform/android/java/editor/src/main/java/org/godotengine/editor/BaseGodotEditor.kt
@@ -53,6 +53,7 @@ import org.godotengine.godot.error.Error
 import org.godotengine.godot.utils.PermissionsUtil
 import org.godotengine.godot.utils.ProcessPhoenix
 import org.godotengine.godot.utils.isHorizonOSDevice
+import org.godotengine.godot.utils.isPicoOSDevice
 import org.godotengine.godot.utils.isNativeXRDevice
 import java.util.*
 import kotlin.math.min
@@ -519,6 +520,10 @@ abstract class BaseGodotEditor : GodotActivity() {
 
 		if (featureTag == "horizonos") {
 			return isHorizonOSDevice()
+		}
+
+		if (featureTag == "picoos") {
+			return isPicoOSDevice()
 		}
 
         return false

platform/android/java/editor/src/picoos/java/org/godotengine/editor/GodotEditor.kt
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  DeviceUtils.kt                                                        */
+/*  GodotEditor.kt                                                        */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -28,32 +28,55 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-/**
- * Contains utility methods for detecting specific devices.
- */
-@file:JvmName("DeviceUtils")
+package org.godotengine.editor
 
-package org.godotengine.godot.utils
-
-import android.os.Build
+import org.godotengine.godot.GodotLib
+import org.godotengine.godot.utils.isNativeXRDevice
 
 /**
- * Returns true if running on Meta Horizon OS.
+ * Primary window of the Godot Editor.
+ *
+ * This is the implementation of the editor used when running on PicoOS devices.
  */
-fun isHorizonOSDevice(): Boolean {
-	return "Oculus".equals(Build.BRAND, true)
-}
+open class GodotEditor : BaseGodotEditor() {
 
-/**
- * Returns true if running on Meta Horizon OS.
- */
-fun isPicoOSDevice(): Boolean {
-	return ("Pico".equals(Build.BRAND, true))
-}
+	companion object {
+		private val TAG = GodotEditor::class.java.simpleName
 
-/**
- * Returns true if running on a native Android XR device.
- */
-fun isNativeXRDevice(): Boolean {
-	return isHorizonOSDevice() || isPicoOSDevice()
+		internal val XR_RUN_GAME_INFO = EditorWindowInfo(GodotXRGame::class.java, 1667, ":GodotXRGame")
+	}
+
+	override fun retrieveEditorWindowInfo(args: Array<String>): EditorWindowInfo {
+		var hasEditor = false
+		var xrModeOn = false
+
+		var i = 0
+		while (i < args.size) {
+			when (args[i++]) {
+				EDITOR_ARG, EDITOR_ARG_SHORT, EDITOR_PROJECT_MANAGER_ARG, EDITOR_PROJECT_MANAGER_ARG_SHORT -> hasEditor = true
+				XR_MODE_ARG -> {
+					val argValue = args[i++]
+					xrModeOn = xrModeOn || ("on" == argValue)
+				}
+			}
+		}
+
+		return if (hasEditor) {
+			EDITOR_MAIN_INFO
+		} else {
+			val openxrEnabled = GodotLib.getGlobal("xr/openxr/enabled").toBoolean()
+			if (openxrEnabled && isNativeXRDevice()) {
+				XR_RUN_GAME_INFO
+			} else {
+				RUN_GAME_INFO
+			}
+		}
+	}
+
+	override fun getEditorWindowInfoForInstanceId(instanceId: Int): EditorWindowInfo? {
+		return when (instanceId) {
+			XR_RUN_GAME_INFO.windowId -> XR_RUN_GAME_INFO
+			else -> super.getEditorWindowInfoForInstanceId(instanceId)
+		}
+	}
 }

platform/android/java/editor/src/picoos/java/org/godotengine/editor/GodotXRGame.kt
@@ -1,0 +1,59 @@
+/*************************************************************************/
+/*  GodotXRGame.kt                                                       */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2022 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2022 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+package org.godotengine.editor
+
+import org.godotengine.godot.GodotLib
+import org.godotengine.godot.xr.XRMode
+
+/**
+ * Provide support for running XR apps / games from the editor window.
+ */
+open class GodotXRGame: GodotGame() {
+
+	override fun overrideOrientationRequest() = true
+
+	override fun updateCommandLineParams(args: List<String>) {
+		val updatedArgs = ArrayList<String>()
+		if (!args.contains(XRMode.OPENXR.cmdLineArg)) {
+			updatedArgs.add(XRMode.OPENXR.cmdLineArg)
+		}
+		if (!args.contains(XR_MODE_ARG)) {
+			updatedArgs.add(XR_MODE_ARG)
+			updatedArgs.add("on")
+		}
+		updatedArgs.addAll(args)
+
+		super.updateCommandLineParams(updatedArgs)
+	}
+
+	override fun getEditorWindowInfo() = XR_RUN_GAME_INFO
+
+}

blazium-engine/blazium

platform/android/java/editor/src/main/java/org/godotengine/editor/BaseGodotEditor.kt
@@ -53,6 +53,7 @@ import app.blazium.godot.error.Error
 import app.blazium.godot.utils.PermissionsUtil
 import app.blazium.godot.utils.ProcessPhoenix
 import app.blazium.godot.utils.isHorizonOSDevice
+import app.blazium.godot.utils.isPicoOSDevice
 import app.blazium.godot.utils.isNativeXRDevice
 import java.util.*
 import kotlin.math.min
@@ -519,6 +520,10 @@ abstract class BaseGodotEditor : GodotActivity() {
 
 		if (featureTag == "horizonos") {
 			return isHorizonOSDevice()
+		}
+
+		if (featureTag == "picoos") {
+			return isPicoOSDevice()
 		}
 
         return false

platform/android/java/editor/src/picoos/java/org/godotengine/editor/GodotEditor.kt
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  DeviceUtils.kt                                                        */
+/*  GodotEditor.kt                                                        */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -28,32 +28,55 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-/**
- * Contains utility methods for detecting specific devices.
- */
-@file:JvmName("DeviceUtils")
+package app.blazium.editor
 
-package app.blazium.godot.utils
-
-import android.os.Build
+import app.blazium.godot.GodotLib
+import app.blazium.godot.utils.isNativeXRDevice
 
 /**
- * Returns true if running on Meta Horizon OS.
+ * Primary window of the Godot Editor.
+ *
+ * This is the implementation of the editor used when running on PicoOS devices.
  */
-fun isHorizonOSDevice(): Boolean {
-	return "Oculus".equals(Build.BRAND, true)
-}
+open class GodotEditor : BaseGodotEditor() {
 
-/**
- * Returns true if running on PICO OS.
- */
-fun isPicoOSDevice(): Boolean {
-	return ("Pico".equals(Build.BRAND, true))
-}
+	companion object {
+		private val TAG = GodotEditor::class.java.simpleName
 
-/**
- * Returns true if running on a native Android XR device.
- */
-fun isNativeXRDevice(): Boolean {
-	return isHorizonOSDevice() || isPicoOSDevice()
+		internal val XR_RUN_GAME_INFO = EditorWindowInfo(GodotXRGame::class.java, 1667, ":GodotXRGame")
+	}
+
+	override fun retrieveEditorWindowInfo(args: Array<String>): EditorWindowInfo {
+		var hasEditor = false
+		var xrModeOn = false
+
+		var i = 0
+		while (i < args.size) {
+			when (args[i++]) {
+				EDITOR_ARG, EDITOR_ARG_SHORT, EDITOR_PROJECT_MANAGER_ARG, EDITOR_PROJECT_MANAGER_ARG_SHORT -> hasEditor = true
+				XR_MODE_ARG -> {
+					val argValue = args[i++]
+					xrModeOn = xrModeOn || ("on" == argValue)
+				}
+			}
+		}
+
+		return if (hasEditor) {
+			EDITOR_MAIN_INFO
+		} else {
+			val openxrEnabled = GodotLib.getGlobal("xr/openxr/enabled").toBoolean()
+			if (openxrEnabled && isNativeXRDevice()) {
+				XR_RUN_GAME_INFO
+			} else {
+				RUN_GAME_INFO
+			}
+		}
+	}
+
+	override fun getEditorWindowInfoForInstanceId(instanceId: Int): EditorWindowInfo? {
+		return when (instanceId) {
+			XR_RUN_GAME_INFO.windowId -> XR_RUN_GAME_INFO
+			else -> super.getEditorWindowInfoForInstanceId(instanceId)
+		}
+	}
 }

platform/android/java/editor/src/picoos/java/org/godotengine/editor/GodotXRGame.kt
@@ -1,0 +1,59 @@
+/*************************************************************************/
+/*  GodotXRGame.kt                                                       */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2022 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2022 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+package app.blazium.editor
+
+import app.blazium.godot.GodotLib
+import app.blazium.godot.xr.XRMode
+
+/**
+ * Provide support for running XR apps / games from the editor window.
+ */
+open class GodotXRGame: GodotGame() {
+
+	override fun overrideOrientationRequest() = true
+
+	override fun updateCommandLineParams(args: List<String>) {
+		val updatedArgs = ArrayList<String>()
+		if (!args.contains(XRMode.OPENXR.cmdLineArg)) {
+			updatedArgs.add(XRMode.OPENXR.cmdLineArg)
+		}
+		if (!args.contains(XR_MODE_ARG)) {
+			updatedArgs.add(XR_MODE_ARG)
+			updatedArgs.add("on")
+		}
+		updatedArgs.addAll(args)
+
+		super.updateCommandLineParams(updatedArgs)
+	}
+
+	override fun getEditorWindowInfo() = XR_RUN_GAME_INFO
+
+}