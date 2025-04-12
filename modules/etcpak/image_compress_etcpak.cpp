KleirRampage45/redot-engine

Etcpak: Sync with upstream

modules/etcpak/image_compress_etcpak.cpp
@@ -276,12 +276,12 @@ void _compress_etcpak(EtcpakType p_compress_type, Image *r_img) {
 				break;
 
 			case EtcpakType::ETCPAK_TYPE_DXT1:
-				CompressDxt1Dither(src_mip_read, dest_mip_write, blocks, dest_mip_w);
+				CompressBc1Dither(src_mip_read, dest_mip_write, blocks, dest_mip_w);
 				break;
 
 			case EtcpakType::ETCPAK_TYPE_DXT5:
 			case EtcpakType::ETCPAK_TYPE_DXT5_RA_AS_RG:
-				CompressDxt5(src_mip_read, dest_mip_write, blocks, dest_mip_w);
+				CompressBc3(src_mip_read, dest_mip_write, blocks, dest_mip_w);
 				break;
 
 			case EtcpakType::ETCPAK_TYPE_RGTC_R:

blazium-engine/blazium

modules/etcpak/image_compress_etcpak.cpp
@@ -253,12 +253,12 @@ void _compress_etcpak(EtcpakType p_compresstype, Image *r_img) {
 				break;
 
 			case EtcpakType::ETCPAK_TYPE_DXT1:
-				CompressDxt1Dither(src_mip_read, dest_mip_write, blocks, mip_w);
+				CompressBc1Dither(src_mip_read, dest_mip_write, blocks, mip_w);
 				break;
 
 			case EtcpakType::ETCPAK_TYPE_DXT5:
 			case EtcpakType::ETCPAK_TYPE_DXT5_RA_AS_RG:
-				CompressDxt5(src_mip_read, dest_mip_write, blocks, mip_w);
+				CompressBc3(src_mip_read, dest_mip_write, blocks, mip_w);
 				break;
 
 			case EtcpakType::ETCPAK_TYPE_RGTC_R: