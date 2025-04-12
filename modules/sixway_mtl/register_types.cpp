7342032faef6a410d35ce8cd3b7ab2d7c5e80062
TheAenema <thegreatnone1995@gmail.com>
2025-04-12 14:32:24 +0330

    Six-Way Lighting Material Formatting Fix

--- modules/sixway_mtl/register_types.cpp
+++ modules/sixway_mtl/register_types.cpp
@@ -5,16 +5,16 @@
 #include "register_types.h"
 
 // Module Routines
-void initialize_sixway_mtl_module(ModuleInitializationLevel p_level) 
-{
+void initialize_sixway_mtl_module(ModuleInitializationLevel p_level) {
 	// Check for Initialization Level
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+		return;
 
 	// Initialize Module
 	SixWayLightingMaterial::RegisterModule();
 }
-void uninitialize_sixway_mtl_module(ModuleInitializationLevel p_level)
-{
+void uninitialize_sixway_mtl_module(ModuleInitializationLevel p_level) {
 	// Check for Initialization Level
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) return;
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+		return;
 }
--- modules/sixway_mtl/register_types.h
+++ modules/sixway_mtl/register_types.h
@@ -2,13 +2,13 @@
 /*   Six-Way Volumetric Lighting for Blazium v2.0   */
 /*            Developed by Hamid.Memar              */
 
-#ifndef HM_SIX_WAY_LIGHTING_SHADER_TYPES_H
-#define HM_SIX_WAY_LIGHTING_SHADER_TYPES_H
+#ifndef SIXWAY_MTL_REGISTER_TYPES_H
+#define SIXWAY_MTL_REGISTER_TYPES_H
 
-#include "sixway_mtl.h"
 #include "modules/register_module_types.h"
+#include "sixway_mtl.h"
 
 void initialize_sixway_mtl_module(ModuleInitializationLevel p_level);
 void uninitialize_sixway_mtl_module(ModuleInitializationLevel p_level);
 
-#endif // HM_SIX_WAY_LIGHTING_SHADER_TYPES_H
+#endif // SIXWAY_MTL_REGISTER_TYPES_H
