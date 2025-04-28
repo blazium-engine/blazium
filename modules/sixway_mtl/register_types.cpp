27c8a5214b54573151f4aac50318f50217b4eb04
sshiiden <santos.shiden.nicholas@gmail.com>
2025-04-28 16:48:20 +0200

    Fix sixway_mtl formatting

--- modules/sixway_mtl/register_types.cpp
+++ modules/sixway_mtl/register_types.cpp
@@ -7,14 +7,16 @@
 // Module Routines
 void initialize_sixway_mtl_module(ModuleInitializationLevel p_level) {
 	// Check for Initialization Level
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
+	}
 
 	// Initialize Module
 	SixWayLightingMaterial::RegisterModule();
 }
 void uninitialize_sixway_mtl_module(ModuleInitializationLevel p_level) {
 	// Check for Initialization Level
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
+	}
 }
