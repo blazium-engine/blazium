18dc70b226703bc6234e43a9c3d0f581d8d12e13
Dragos Daian <daian.dragos@yahoo.com>
2025-06-10 22:31:31 +0200

    upd123

--- modules/gdterm/gdterm/pty/platform/windows/pty_proxy_win.h
+++ modules/gdterm/gdterm/pty/platform/windows/pty_proxy_win.h
@@ -4,8 +4,8 @@
 #include "../../pty_proxy.h"
 
 #define WIN32_LEAN_AND_MEAN
+#define _WIN32_WINNT 0x0A00
 #include <wincon.h>
-#include <windef.h>
 #include <windows.h>
 #include <condition_variable>
 #include <mutex>
