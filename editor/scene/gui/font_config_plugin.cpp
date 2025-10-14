godotengine/godot

Cleanup EditorProperty setup methods

editor/plugins/font_config_plugin.cpp
@@ -35,6 +35,7 @@
 #include "editor/import/dynamic_font_import_settings.h"
 #include "editor/themes/editor_scale.h"
 #include "scene/gui/margin_container.h"
+#include "tiles/tile_set_editor.h"
 
 /*************************************************************************/
 /*  EditorPropertyFontMetaObject                                         */
@@ -471,7 +472,12 @@ void EditorPropertyOTVariation::update_property() {
 			Vector3i range = supported.get_value_at_index(i);
 
 			EditorPropertyInteger *prop = memnew(EditorPropertyInteger);
-			prop->setup(range.x, range.y, false, true, false, false);
+			EditorPropertyRangeHint hint;
+			hint.min = range.x;
+			hint.max = range.y;
+			hint.or_greater = false;
+			hint.or_less = false;
+			prop->setup(hint);
 			prop->set_object_and_property(object.ptr(), "keys/" + itos(name_tag));
 
 			String name = TS->tag_to_name(name_tag);
@@ -764,7 +770,13 @@ void EditorPropertyOTFeatures::update_property() {
 					} break;
 					case Variant::INT: {
 						EditorPropertyInteger *editor = memnew(EditorPropertyInteger);
-						editor->setup(0, 255, 1, false, false, false);
+						EditorPropertyRangeHint hint;
+						hint.min = 0;
+						hint.max = 255;
+						hint.hide_slider = false;
+						hint.or_greater = false;
+						hint.or_less = false;
+						editor->setup(hint);
 						prop = editor;
 					} break;
 					default: {

blazium-engine/blazium

editor/scene/gui/font_config_plugin.cpp
@@ -461,7 +461,12 @@ void EditorPropertyOTVariation::update_property() {
 			Vector3i range = supported.get_value_at_index(i);
 
 			EditorPropertyInteger *prop = memnew(EditorPropertyInteger);
-			prop->setup(range.x, range.y, 1, false, true, false, false);
+			EditorPropertyRangeHint hint;
+			hint.min = range.x;
+			hint.max = range.y;
+			hint.or_greater = false;
+			hint.or_less = false;
+			prop->setup(hint);
 			prop->set_object_and_property(object.ptr(), "keys/" + itos(name_tag));
 
 			String name = TS->tag_to_name(name_tag);
@@ -740,7 +745,13 @@ void EditorPropertyOTFeatures::update_property() {
 					} break;
 					case Variant::INT: {
 						EditorPropertyInteger *editor = memnew(EditorPropertyInteger);
-						editor->setup(0, 255, 1, false, false, false, false);
+						EditorPropertyRangeHint hint;
+						hint.min = 0;
+						hint.max = 255;
+						hint.hide_control = false;
+						hint.or_greater = false;
+						hint.or_less = false;
+						editor->setup(hint);
 						prop = editor;
 					} break;
 					default: {