9444c6622fdc64bf859668af1877af53ed14b916
jss2a98aj <jss2a98aj@gmail.com>
2025-10-16 20:13:25 -0400

    Add missing newline to set_copyright_headers.py

--- misc/scripts/copyright_headers.py
+++ misc/scripts/copyright_headers.py
@@ -40,7 +40,8 @@ license_text = """\
 /* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
 /* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
-/**************************************************************************/"""
+/**************************************************************************/
+"""
 
 # Parse command line arguments
 use_blazium = "--blazium" in sys.argv
