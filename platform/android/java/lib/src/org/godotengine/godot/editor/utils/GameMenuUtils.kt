syntaxerror247/godot

Android Editor: Add game speed control options in game menu bar

platform/android/java/lib/src/org/godotengine/godot/editor/utils/GameMenuUtils.kt
@@ -95,6 +95,13 @@ object GameMenuUtils {
 	@JvmStatic
 	external fun setDebugMuteAudio(enabled: Boolean)
 
+	@JvmStatic
+	external fun resetTimeScale()
+
+	@JvmStatic
+	external fun setTimeScale(multiplier: Double)
+
+
 	/**
 	 * Returns [GameEmbedMode] stored in the editor settings.
 	 *

blazium-engine/blazium

platform/android/java/lib/src/org/godotengine/godot/editor/utils/GameMenuUtils.kt
@@ -95,6 +95,13 @@ object GameMenuUtils {
 	@JvmStatic
 	external fun setDebugMuteAudio(enabled: Boolean)
 
+	@JvmStatic
+	external fun resetTimeScale()
+
+	@JvmStatic
+	external fun setTimeScale(scale: Double)
+
+
 	/**
 	 * Returns [GameEmbedMode] stored in the editor settings.
 	 *