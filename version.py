3652a4155937ba27edd5deb0a5a4f1593f2d8b3d
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2025-12-07 07:07:39 -0600

    Android export template - version.py simplication
    Android gradle configs don't seem to like my adjustments to version.py.  I'm removing the comments and switching from None to an empty string and will retest.

--- version.py
+++ version.py
@@ -14,10 +14,4 @@ external_status = "dev"
 external_sha = "876b290332ec6f2e6d173d08162a02aa7e6ca46d"
 mirror_list = "https://blazium.app/api/mirrorlist/"
 version_url = "https://blazium.app/api/versions-" + external_status + ".json"
-# XOR obfuscation key for pack files
-# Set to None or empty string to disable functionality.
-# To generate functional key, which has to be exactly
-# 1024 bytes, run the following and set the output as
-# a string value inside blazium_xor_key variable:
-# $ tr -dc 'A-Za-z0-9' </dev/urandom | head -c 1024
-blazium_xor_key = None
+blazium_xor_key = ""
